06e0bd646a92f861c85c0dfa680710ae013fafec
MB <michael.dburke56@gmail.com>
2022-09-28 10:25:16 -0500

    Update main lua nvim conf file.

--- init.lua
+++ init.lua
@@ -1,4 +1,5 @@
 require("user.plugins")
+require("treesitter-config")
 
 vim.bo.expandtab = true
 vim.bo.shiftwidth = 4
@@ -7,6 +8,9 @@ vim.bo.tabstop = 4
 
 vim.wo.scrolloff = 15
 vim.wo.colorcolumn = "80"
+vim.wo.relativenumber = true
+vim.wo.nu = true
+vim.wo.wrap = false
 
 vim.o.background = "dark" 
 vim.cmd([[colorscheme gruvbox]])
